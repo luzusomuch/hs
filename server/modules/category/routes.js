1c81fa732cedcec915a3617e86f1e713d4bf53bb
Vinh Nguyen <luzusomuch@gmail.com>
2016-06-23 17:45:47 +0700

    [+] add route to create new category for admin but we need create code to upload file to s3 to completed this function

--- server/modules/category/routes.js
+++ server/modules/category/routes.js
@@ -1,12 +1,17 @@
 import Joi from 'joi'
 module.exports = function(kernel) {
-	kernel.app.post('/api/v1/categories/', kernel.middleware.isAuthenticated(), (req, res) => {  
-    let model = new kernel.model.Like({objectName: req.params.objectName, objectId: req.params.objectId,  ownerId: req.user._id});
-    model.save().then(like => {
-      res.status(200).json(like);
-    })
-    .catch(err => {
-      res.status(500).end();
+	kernel.app.post('/api/v1/categories/', kernel.middleware.hasRole('admin'), (req, res) => {  
+		// TODO- create a function to upload file to s3
+		var s3PublicUrl = "s3PublicUrl";
+    let model = new kernel.model.Category({
+    	name: req.body.name, 
+    	description: req.body.description,
+    	imagePath: s3PublicUrl
+    });
+    model.save().then(category => {
+      res.status(200).json(category);
+    }).catch(err => {
+      res.status(500).json(err);
     });
   });
 };

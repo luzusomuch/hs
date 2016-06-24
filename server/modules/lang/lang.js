ee726e9e7010d912ffd5858a6a573eb6ebb697ad
Vinh Nguyen <luzusomuch@gmail.com>
2016-06-24 09:29:43 +0700

    [+] local sign in

--- server/modules/lang/lang.js
+++ server/modules/lang/lang.js
@@ -34,7 +34,8 @@ let Language = {
 		REQUIRED_ERROR: 'This field is required',
 		EMAIL_TYPE_ERROR: 'Your email is not valid',
 		PASSWORD_MIN_LENGTH_ERROR: 'Minimun length of password is 8 characters',
-		CONFIRM_PASSWORD_NOT_MATCH: 'Password and confirm password not match'
+		CONFIRM_PASSWORD_NOT_MATCH: 'Password and confirm password not match',
+		SIGN_IN_ERROR: 'Password or email not correct'
 	},
 	de: {
 

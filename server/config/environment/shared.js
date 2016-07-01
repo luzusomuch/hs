1382b51df670ee7728cb13a0126a1a08553b83fc
Trung Le <htle1810@gmail.com>
2016-07-01 13:36:09 +0700

    add more config

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -4,5 +4,9 @@ exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
   baseUrl: 'http://healthstars.dev/',
-  apiVer : 'v1'
+  apiVer : 'v1',
+  apiKey: {
+  	google: 'AIzaSyBhg6JbSzbbB1rC9iqukJdiAXzUOYlpf-4',
+  	weather: 'd6ce4efe26d8a70511337db70401d39c'
+  }
 };

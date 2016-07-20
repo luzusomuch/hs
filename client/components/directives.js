c580b5e4beb726293d29c4e9d96285b2c4de8b57
Vinh Nguyen <luzusomuch@gmail.com>
2016-07-20 15:27:32 +0700

    [*] change socket.io position in html

--- client/components/directives.js
+++ client/components/directives.js
@@ -9,8 +9,6 @@ angular.module('healthStarsApp')
     link: function(scope, element, attributes, modelVal) {
 
       modelVal.$validators.compareTo = function(val) {
-        console.log(val);
-        console.log(scope.confirmPassword);
         return val === scope.confirmPassword;
       };
 

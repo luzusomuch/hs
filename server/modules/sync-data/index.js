5a729d25f1c736e5c906f43913345190e9707434
Trung Le <htle1810@gmail.com>
2016-07-21 15:24:30 +0700

    fix sync data

--- server/modules/sync-data/index.js
+++ server/modules/sync-data/index.js
@@ -1,25 +1,43 @@
 'use strict';
 import async from 'async';
-import kernel from '../../app';
+import KernelFactory from './../../kernel';
+import es from './../../modules/es';
+import Mapping from './../../modules/es/mapping';
 
-async.parallel([
-		(cb) => {
-			// sync event
-			kernel.model.Event.find({}).then(events => {
-				async.each(events, (event, callback) => {
-					kernel.ES.create({
-						type: kernel.ES.config.mapping.eventType, 
-						id: event._id.toString(),
-						data: event.toJSON()
-					}, (err, result) => {
-						if (err) {
-							console.log(err);
-						}
-						callback()
-					});
-				}, cb);
-			}).catch(cb);
+let config = require('./../../config/environment');
+let kernel = new KernelFactory(config);
+let m = new Mapping(es.config.ES);
+kernel.ES = new es.HealthStarsES(es.config.ES, m.mapping);
+
+
+kernel.loadModule(require('./../../modules/event'));
+kernel.compose();
+kernel.ES.index((err) => {
+		if(err) {
+		  console.log(err);
+		  process.exit(0);
+		  return;
 		}
-	], () => {
-		console.log("SYNC DATA DONE");
-	});
+		async.parallel([
+			(cb) => {
+				// sync event
+				kernel.model.Event.find({}).then(events => {
+					async.each(events, (event, callback) => {
+						kernel.ES.create({
+							type: kernel.ES.config.mapping.eventType, 
+							id: event._id.toString(),
+							data: event.toJSON()
+						}, (err, result) => {
+							if (err) {
+								console.log(err);
+							}
+							callback()
+						});
+					}, cb);
+				}).catch(cb);
+			}
+		], () => {
+			console.log("SYNC DATA DONE");
+			process.exit(0);
+		});
+});

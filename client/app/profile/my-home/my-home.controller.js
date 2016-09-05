78c0fd504de4dd33f392f8a340b88c2d49c9bb27
hanh doan <hanhhuudoan@gmail.com>
2016-09-05 17:37:17 +0700

    [-] fix style for comment

--- client/app/profile/my-home/my-home.controller.js
+++ client/app/profile/my-home/my-home.controller.js
@@ -156,14 +156,10 @@ class MyHomeCtrl {
 		        }).then(response => {
 		            	console.log(response.data);
 		            }, (resp) => {
-		            	console.log('error when get contact');
-						console.log(resp);
 		            	this.growl.error(`<p>{{'SOMETHING_WENT_WRONG' | translate}}</p>`);
 		            }
 		        );
 			}, (resp) => {
-				console.log('error when login');
-				console.log(resp);
 				this.growl.error(`<p>{{'SOMETHING_WENT_WRONG' | translate}}</p>`);
 			});
 		}

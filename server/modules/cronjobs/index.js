1d6ff4c93b1babe69e6c1cdebee32b3a9f36ccad
Vinh Nguyen <luzusomuch@gmail.com>
2016-10-24 16:08:48 +0700

    [-] change cron

--- server/modules/cronjobs/index.js
+++ server/modules/cronjobs/index.js
@@ -1,6 +1,21 @@
+'use strict';
 import async from 'async';
-import kernel from '../../app';
+import _ from 'lodash';
+import KernelFactory from './../../kernel';
+import es from './../../modules/es';
+import Mapping from './../../modules/es/mapping';
 
+let config = require('./../../config/environment');
+let kernel = new KernelFactory(config);
+let m = new Mapping(es.config.ES);
+kernel.ES = new es.HealthStarsES(es.config.ES, m.mapping);
+
+kernel.loadModule(require('./../../modules/event'));
+kernel.loadModule(require('./../../modules/user'));
+kernel.loadModule(require('./../../modules/like'));
+kernel.loadModule(require('./../../modules/invitation-request'));
+kernel.loadModule(require('./../../modules/feed'));
+kernel.compose();
 async.parallel([
 	(cb) => {
 		require('./processEvent')(kernel, cb);

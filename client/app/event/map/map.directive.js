26547e08213a18f76eca3d1b48e4f804f331ecf9
Vinh Nguyen <luzusomuch@gmail.com>
2017-03-22 09:03:45 +0700

    tab index

--- client/app/event/map/map.directive.js
+++ client/app/event/map/map.directive.js
@@ -20,35 +20,36 @@ angular.module('healthStarsApp').directive('hsEventMap', ($interval) => {
 		link: function(scope, elm) {
 			scope.address = '';
 			var map, google;
-	    var mapElm = angular.element(elm).find('.event-map');
-	    var markers = [];
-	    var initMap = function(locations, center) {
-	    	center = center || {lat: 52.511, lng: 13.447};
-	      map = new google.maps.Map(mapElm[0], {
-	        center: center,
-	        zoom: 5
-	      });
-	      var resize = false;
-	      if(typeof locations === 'object' && locations.length) {
-	      	if(locations.length === 1 && locations[0].fullAddress) {
-	      		scope.address = locations[0].fullAddress;
-	      	}
-	      	var bounds = new google.maps.LatLngBounds();
-	      	_.each(locations, function(location) {
-	      		let pos = (typeof location === 'object' &&  location.coordinates)? location.coordinates : null;
-	      		if(pos) {
-	      			var latLng = new google.maps.LatLng(pos[1], pos[0]);
-			        var marker = new google.maps.Marker({
-			            map: map,
-			            position: latLng
-			        });
-			        markers.push(marker);
-			        bounds.extend(latLng);
-			        resize = true;
-		        }
-	        });
-	        if(resize) {
-		        map.setCenter(bounds.getCenter());
+	    	var mapElm = angular.element(elm).find('.event-map');
+	    	var markers = [];
+	    	var initMap = function(locations, center) {
+	    		center = center || {lat: 52.511, lng: 13.447};
+	      		map = new google.maps.Map(mapElm[0], {
+        		center: center,
+	        	zoom: 5
+	      	});
+	      	var resize = false;
+
+	      	if(typeof locations === 'object' && locations.length) {
+	      		if(locations.length === 1 && locations[0].fullAddress) {
+	      			scope.address = locations[0].fullAddress;
+	      		}
+	      		var bounds = new google.maps.LatLngBounds();
+	      		_.each(locations, function(location) {
+		      		let pos = (typeof location === 'object' &&  location.coordinates)? location.coordinates : null;
+		      		if(pos) {
+		      			var latLng = new google.maps.LatLng(pos[1], pos[0]);
+				        var marker = new google.maps.Marker({
+				            map: map,
+				            position: latLng
+				        });
+				        markers.push(marker);
+				        bounds.extend(latLng);
+				        resize = true;
+			        }
+		        });
+		        if(resize) {
+			        map.setCenter(bounds.getCenter());
 						map.fitBounds(bounds);
 						var listener = google.maps.event.addListener(map, 'idle', function() { 
 						  if (map.getZoom() > 15) { 
@@ -57,31 +58,32 @@ angular.module('healthStarsApp').directive('hsEventMap', ($interval) => {
 						  google.maps.event.removeListener(listener);
 						});
 					}
-	      }
-	    };
-	    scope.$watch('locations', (nv) => {
-    		for(var i=0; i< markers.length; i++) {
-    			markers[i].setMap(null);
-    		}
-    		markers = [];
-	    	if (nv && nv[0]) {
-		    	var $ttl = $interval(() => {
-		    		if(window.google && window.google.maps) {
-		    			$interval.cancel($ttl);
-		    			google = window.google;
-		    			if (scope.place==='create-event') {
-		    				let data = [{
-		    					coordinates: [nv[0].geometry.location.lng, nv[0].geometry.location.lat],
-		    					fullAddress: nv[0].formatted_address
-		    				}];
-		    				initMap(data, scope.center);
-		    			} else {
-		    				initMap(nv, scope.center);
-		    			}
-		    		}
-		    	}, 250);
-	    	}
-	    });
+	      		}
+	   		};
+
+		    scope.$watch('locations', (nv) => {
+	    		for(var i=0; i< markers.length; i++) {
+	    			markers[i].setMap(null);
+	    		}
+	    		markers = [];
+		    	if (nv && nv[0]) {
+			    	var $ttl = $interval(() => {
+			    		if(window.google && window.google.maps) {
+			    			$interval.cancel($ttl);
+			    			google = window.google;
+			    			if (scope.place==='create-event') {
+			    				let data = [{
+			    					coordinates: [nv[0].geometry.location.lng, nv[0].geometry.location.lat],
+			    					fullAddress: nv[0].formatted_address
+			    				}];
+			    				initMap(data, scope.center);
+			    			} else {
+			    				initMap(nv, scope.center);
+			    			}
+			    		}
+			    	}, 250);
+		    	}
+		    });
 		}
 	};
 }).controller('EventMapCtrl', EventMapCtrl);

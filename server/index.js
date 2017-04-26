d94e6fd24a5fe4472d19b85d0c4d51fb658c9137
Vinh Nguyen <luzusomuch@gmail.com>
2017-04-26 09:48:33 +0700

    test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Set default node environment to development
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //bluebird config
 process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
 process.env.BLUEBIRD_WARNINGS = 0;
 
-if (env === 'development' || env === 'test' || env==='production') {
+if (env === 'development' || env === 'test') {
   // Register the Babel require hook
   require('babel-register');
 }

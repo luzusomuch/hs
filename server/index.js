5ea21c2a38339ba7e9d626dd7104ca1bd3b9eb66
Vinh Nguyen <luzusomuch@gmail.com>
2017-04-26 09:42:50 +0700

    gulp babel add production

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Set default node environment to development
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 //bluebird config
 process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
 process.env.BLUEBIRD_WARNINGS = 0;

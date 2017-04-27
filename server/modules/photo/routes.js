e57aef565e267a3a26101e8691ba75cab6fe2e07
Vinh Nguyen <luzusomuch@gmail.com>
2017-04-27 17:13:45 +0700

    remove send email when block/unblock and delete photo

--- server/modules/photo/routes.js
+++ server/modules/photo/routes.js
@@ -474,7 +474,7 @@ module.exports = function(kernel) {
 				photo.blocked = !photo.blocked;
 				photo.blockedBy = (photo.blocked) ? req.user._id : null;
 				photo.save().then(photo => {
-					kernel.queue.create('EMAIL_BLOCK_UNBLOCKED_PHOTO', {photoId: photo._id}).save();
+					//kernel.queue.create('EMAIL_BLOCK_UNBLOCKED_PHOTO', {photoId: photo._id}).save();
 					return res.status(200).json({blocked: photo.blocked});
 				}).catch(err => {
 					return res.status(500).json({type: 'SERVER_ERROR'});
@@ -493,7 +493,7 @@ module.exports = function(kernel) {
 			}
 			if (photo.ownerId.toString()===req.user._id.toString()) {
 				photo.remove().then(() => {
-					kernel.queue.create('DELETE_PHOTO', photo).save();
+					//kernel.queue.create('DELETE_PHOTO', photo).save();
 					return res.status(200).end();
 				}).catch(err => {
 					return res.status(500).json({type: 'SERVER_ERROR'});
